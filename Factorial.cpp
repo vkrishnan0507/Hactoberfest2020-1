9094b9c6b0803024e009e579baf9e404b0ee658a
Vanitha K <59090053+vkrishnan0507@users.noreply.github.com>
2020-10-05 16:30:35 +0530

    Update Factorial.cpp

--- Factorial.cpp
+++ Factorial.cpp
@@ -4,10 +4,10 @@
 void main()
 {
 int i, no, fact=1;
-clrscr();
+clrscr(); //to clear the console
 cout<<"Enter the any no. : ";
 cin>>no;
-for(i=1;i<=no;i++)
+for(i=1;i<=no;i++) //loop for calculating factorial(if you have entered 4, then 4=4*1 
 {
 fact=fact*i;
 }
